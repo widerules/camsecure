443987620b10510cf705275d259d6560003f6e2c
ronald.stevenson666@gmail.com <ronald.stevenson666@gmail.com@6411e1cb-1765-6531-68e1-a571c388000e>
2011-05-22 11:41:05 +0000

    App delegate

---
+++ branches/RALPHAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RALPHAppDelegate.h
+//  RALPH
+//
+//  Created by Ronald on 23/04/11.
+//  by Ronald
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RALPHAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
